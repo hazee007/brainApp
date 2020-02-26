60efd416bed3e862ab21fb704ac0dc04707ed95f
hazee007 <ajimoh2012@yahoo.com>
2020-02-26 13:59:27 +0400

    changing port

--- server.js
+++ server.js
@@ -47,6 +47,6 @@ app.put('/image' , (req, res) => {entries.getEntries(req, res, db)})
 app.post('/imageUrl' , (req, res) => {entries.handelApiCall(req, res)})
 
 
-app.listen(3001, ()=>{
-    console.log('app is running on port 3001')
+app.listen(process.env.PORT || 3001, ()=>{
+  console.log(`app is running on port 3001 ${process.env.PORT}`)
 })
